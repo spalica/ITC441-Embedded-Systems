076ae236af1f9b44df439e9afc85c2688bafa4cb
Steven Palica <59748859+spalica@users.noreply.github.com>
2021-09-06 12:30:54 -0600

    adding gpio funcionality

--- Lab 1/hello_world.py
+++ Lab 1/hello_world.py
@@ -1,8 +1,46 @@
-import flask
+# set up flask
+
+from flask import Flask
 from flask import request
 from flask import send_from_directory
+app = Flask(__name__)
 
-app = flask(__name__)
+# set things up for GPIO
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+RED = 23
+YELLOW = 24
+GREEN = 25
+
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+
+
+
+
+def lights(light):
+    light = str(light)
+    if light == 'green':
+        GPIO.output(GREEN, True)
+        GPIO.output(YELLOW, False)
+        GPIO.output(RED, False)
+    if light == 'yellow':
+        GPIO.output(GREEN, False)
+        GPIO.output(YELLOW, True)
+        GPIO.output(RED, False)
+    if light == 'red':
+        GPIO.output(GREEN, False)
+        GPIO.output(YELLOW, False)
+        GPIO.output(RED, True)
+    if light == 'off':
+        GPIO.output(GREEN, False)
+        GPIO.output(YELLOW, False)
+        GPIO.output(RED, False)
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -11,10 +49,30 @@ def login():
     else:
         return show_the_login_form()
 
-
+# add route to serve webpage and javascript
 @app.route('/<path:path>', methods=['GET'])
 def home(path):
     return send_from_directory('www', path)
+
+@app.route('/control/', methods=['POST'])
+def control():
+
+    # automatic mode
+    if (request.json.get('mode') == 'auto'):
+        pass
+
+    # manual mode
+    if (request.json.get('mode') == 'manual'):
+        if (request.json.get('color')):
+            lights(request.json.get('color'))
+
+
+    # print('************************************')
+    # print(request)
+    # print(request.json)
+    # print(request.json.get('hi'))
+    return request.json.get('mode')
+    # return request.json['hi']
 
 
 @app.route('/js/<path:path>')
