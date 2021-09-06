677a9b372e0daf00ea836b986c42f3dc1a88c562
Steven Palica <59748859+spalica@users.noreply.github.com>
2021-09-06 13:21:32 -0600

    adding other colors

--- Lab 1/www/control.js
+++ Lab 1/www/control.js
@@ -2,18 +2,22 @@ var url = "control/"
 
 document.getElementById("red").addEventListener("click", function () {
     console.log('red');
-
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     xhr.send(JSON.stringify({"mode": "manual", "color": "red"}));
-
-
-
 });
 document.getElementById("yellow").addEventListener("click", function () {
     console.log('yellow');
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr.send(JSON.stringify({"mode": "manual", "color": "yellow"}));
 });
 document.getElementById("green").addEventListener("click", function () {
     console.log('green');
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr.send(JSON.stringify({"mode": "manual", "color": "green"}));
 });
