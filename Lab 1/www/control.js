003c2301b8ff391a12e637a819ab4080791a808e
Steven Palica <59748859+spalica@users.noreply.github.com>
2021-09-06 13:03:45 -0600

    "

--- Lab 1/www/control.js
+++ Lab 1/www/control.js
@@ -1,7 +1,9 @@
 console.log('working!')
 
+document.getElementById("yellow").addEventListener("click", function() {
+    console.log('Yellow!!');
+  });
 
-
-document.getElementById('yellow').addEventListener(onclick, function() {
-    console.log('Yellow!')
-});
+// document.getElementById('yellow').addEventListener(onclick, function() {
+//     console.log('Yellow!')
+// });
