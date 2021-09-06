fc0a9fd399f86c12ca340c43c9cae45074bd800f
Steven Palica <59748859+spalica@users.noreply.github.com>
2021-09-06 13:31:15 -0600

    cleaning up code

--- Lab 1/www/control.js
+++ Lab 1/www/control.js
@@ -1,19 +1,31 @@
 var url = "control/"
 
-document.getElementById("red").addEventListener("click", function () {
-    console.log('red');
+
+function sendRequest(color){
+    console.log(color);
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-    xhr.send(JSON.stringify({"mode": "manual", "color": "red"}));
+    xhr.send(JSON.stringify({"mode": "manual", "color": color}));
+}
+
+
+document.getElementById("red").addEventListener("click", function () {
+    // console.log('red');
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("POST", url, true);
+    // xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    // xhr.send(JSON.stringify({"mode": "manual", "color": "red"}));
+    sendRequest("red")
 });
 
 document.getElementById("yellow").addEventListener("click", function () {
-    console.log('yellow');
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-    xhr.send(JSON.stringify({"mode": "manual", "color": "yellow"}));
+    // console.log('yellow');
+    // let xhr = new XMLHttpRequest();
+    // xhr.open("POST", url, true);
+    // xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    // xhr.send(JSON.stringify({"mode": "manual", "color": "yellow"}));
+    sendRequest("yellow")
 });
 
 document.getElementById("green").addEventListener("click", function () {
